5075bc7e2f231af82d01eeb9ff67205815885d18
bengjerstad <bengjerstad@gmail.com>
2016-11-04 10:02:37 -0400

    applying fix to issue #9 to this script as well.

--- RemoteAdminFrontEnd/getlog.py
+++ RemoteAdminFrontEnd/getlog.py
@@ -10,18 +10,22 @@ args = sys.argv
 
 r = requests.get('http://10.24.25.130:8000/get_log?username='+args[1]+'&compname=all')
 newtxt = json.loads(r.text)
+if (newtxt=={}): 
+	print("Returned nothing.");
+else:
+	#s  = pandas.Series(newtxt,index=newtxt.keys())
+	df = pd.DataFrame(newtxt)
+	df = df.transpose()
+	#print(df)
+	dt=[]
+	for idx,x in enumerate(df['time']):
+		newx = x.replace("_"," ")
+		#print(newx)
+		dt.append(parser.parse(newx))
+		
 
-#s  = pandas.Series(newtxt,index=newtxt.keys())
-df = pd.DataFrame(newtxt)
-df = df.transpose()
-#print(df)
-dt=[]
-for idx,x in enumerate(df['time']):
-	newx = x.replace("_"," ")
-	dt.append(parser.parse(newx))
-	#print(dt)
-print(df.columns.values)
-del df['time']
-df['date time'] = dt
+	print(df.columns.values)
+	del df['time']
+	df['date time'] = dt
 
-print(df)
+	print(df)
