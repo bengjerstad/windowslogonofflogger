0282103fbc270afff158ae42616f861f2500004a
bengjerstad <bengjerstad@gmail.com>
2017-02-06 08:48:26 -0500

    Removing unnecessary comment

--- startups/wget.js
+++ startups/wget.js
@@ -1,5 +1,3 @@
 var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1"); 
 WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false); WinHttpReq.Send(); 
 WScript.Echo(WinHttpReq.ResponseText); 
-
-/* To save a binary file use this code instead of previous line BinStream = new ActiveXObject("ADODB.Stream"); BinStream.Type = 1; BinStream.Open(); BinStream.Write(WinHttpReq.ResponseBody); BinStream.SaveToFile("out.bin"); */
